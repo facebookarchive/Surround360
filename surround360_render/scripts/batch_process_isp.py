170d20f69772bcbc16bf6ac06ca032ad9abecdfc
Forrest Briggs <fbriggs@fb.com>
2017-01-24 16:38:33 -0800

    make the v2 rendering pipeline store all intermediate images in floating point
    Summary:
    - read in 16 bit input images from the ISP ste
    - keep all intermediate images in floating point [0, 1] representation until final output
    - output 16 bit png at the end
    - verify all extra outputs (mostly for debugging) that can be generated by various flags still work
    - remove old depth estimation code (rather than update it for this change)
    - fix fast preview mono stitcher so it still works after this change
    
    Reviewed By: tox-fb
    
    Differential Revision: D4453789
    
    fbshipit-source-id: e0295226914e7d121f7ef7c3668d022f00a855cb

--- surround360_render/scripts/batch_process_isp.py
+++ surround360_render/scripts/batch_process_isp.py
@@ -25,6 +25,7 @@ RAW2RGB_COMMAND_TEMPLATE = """
 --isp_config_path {ISP_CONFIG_PATH}
 --black_level_offset {BLACK_LEVEL_OFFSET}
 --output_image_path {OUTPUT_IMAGE_PATH}
+--output_bpp 16
 --accelerate
 {FLAGS_RAW2RGB_EXTRA}
 """
